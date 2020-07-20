c99900feb17f5907cc90ae5d426e0445df160320
lanlandetiankong <695605813@qq.com>
2020-07-21 00:32:13 +0800

    try pwd

---
+++ egg-common/src/main/java/com/egg/manager/common/base/enums/bill/BillTypeEnum.java
@@ -0,0 +1,34 @@
+package com.egg.manager.common.base.enums.bill;
+
+public enum BillTypeEnum {
+    Money_In(1,"入账"),
+    Money_Out(2,"出账")
+    ;
+
+    private Integer value ;
+    private String name ;
+
+
+    BillTypeEnum(Integer value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

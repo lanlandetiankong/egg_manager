cf31e4d09bfde5c194b3d22fa13406926436919c
lanlandetiankong <695605813@qq.com>
2020-08-05 14:09:30 +0800

    20200805 引入jasypt配置加密;mysql数据库配置加密

---
+++ egg-common/src/main/java/com/egg/manager/common/util/encrypt/JasyptUtil.java
@@ -0,0 +1,35 @@
+package com.egg.manager.common.util.encrypt;
+
+import org.jasypt.util.text.BasicTextEncryptor;
+
+/**
+ * @Description: spring配置项加密
+ * @ClassName: JasyptUtil
+ * @Author: zhoucj
+ * @Date: 2020/8/5 13:55
+ * @doc https://juejin.im/post/6850418120726446093#comment
+ */
+public class JasyptUtil {
+
+    //必须与application.properties的值一致
+    private static String jasyptEncryptorPassword = "ADUMDFUOV7834*";
+
+    public static void main(String[] args) {
+        encryptMySqlConfig();
+    }
+
+    /**
+     * 取得mysql加密后的配置信息
+     * 相当于java -cp ./jasypt-1.9.3.jar org.jasypt.intf.cli.JasyptPBEStringEncryptionCLI password="ADUMDFUOV7834*" algorithm=PBEWithMD5AndDES input=root
+     */
+    public static void encryptMySqlConfig(){
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        // 加密密钥
+        textEncryptor.setPassword(jasyptEncryptorPassword);
+        // 要加密的数据（如数据库的用户名或密码）
+        String username = textEncryptor.encrypt("root");
+        String password = textEncryptor.encrypt("egg_root");
+        System.out.println("username: " + username);
+        System.out.println("password: " + password);
+    }
+}

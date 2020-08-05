19f8df942ac60356e0d1404a93306972d52aab62
lanlandetiankong <695605813@qq.com>
2020-08-05 09:51:26 +0800

    20200805 Application重复注解

--- egg-web/src/main/java/com/egg/manager/EggManagerApplication.java
+++ egg-web/src/main/java/com/egg/manager/EggManagerApplication.java
@@ -1,15 +1,12 @@
 package com.egg.manager;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
 @EnableCaching
-@EnableAutoConfiguration
 public class EggManagerApplication {
 	public static void main(String[] args) {
         SpringApplication.run(EggManagerApplication.class, args);

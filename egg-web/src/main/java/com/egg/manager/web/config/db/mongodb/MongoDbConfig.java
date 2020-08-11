0a5131df27aed15b27d9a3d1eab0f69dca8395cd
lanlandetiankong <695605813@qq.com>
2020-08-11 09:41:45 +0800

    20200811 mongodb远程连接短时间就会抛UncategorizedMongoDbException异常

---
+++ egg-web/src/main/java/com/egg/manager/web/config/db/mongodb/MongoDbConfig.java
@@ -0,0 +1,24 @@
+package com.egg.manager.web.config.db.mongodb;
+
+import com.mongodb.MongoClientOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * \* note: mongodb 相关配置
+ * \* User: zhouchengjie
+ * \* Date: 2020/8/11
+ * \* Time: 9:36
+ * \* Description:
+ * \
+ */
+@Configuration
+public class MongoDbConfig {
+    @Bean
+    public MongoClientOptions mongoOptions() {
+        return MongoClientOptions
+                .builder()
+                .maxConnectionIdleTime(60000)
+                .build();
+    }
+}

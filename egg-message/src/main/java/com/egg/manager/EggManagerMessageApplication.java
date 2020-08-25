5f61d16f2bb7c8d0f9bc7584195e25d79c2863be
lanlandetiankong <695605813@qq.com>
2020-08-25 16:43:37 +0800

    20200825 todolist; activeMQ-init01

--- egg-message/src/main/java/com/egg/manager/EggManagerMessageApplication.java
+++ egg-message/src/main/java/com/egg/manager/EggManagerMessageApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.jms.annotation.EnableJms;
+
 
 @SpringBootApplication
+@EnableJms
 @EnableCaching
 @EnableDubbo
 @EnableMongoAuditing

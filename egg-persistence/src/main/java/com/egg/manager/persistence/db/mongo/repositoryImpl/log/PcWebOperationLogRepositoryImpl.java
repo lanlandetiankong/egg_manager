c90fc01adcbb02122cabcf6be0303e51c4470525
lanlandetiankong <695605813@qq.com>
2020-09-09 21:49:26 +0800

    20200909 log repository 实现类

---
+++ egg-persistence/src/main/java/com/egg/manager/persistence/db/mongo/repositoryImpl/log/PcWebOperationLogRepositoryImpl.java
@@ -0,0 +1,20 @@
+package com.egg.manager.persistence.db.mongo.repositoryImpl.log;
+
+import com.egg.manager.persistence.db.mongo.mo.log.pc.web.PcWebOperationLogMO;
+import com.egg.manager.persistence.db.mongo.repository.log.pc.web.PcWebOperationLogRepository;
+import com.egg.manager.persistence.db.mongo.repositoryImpl.MyBaseMongoRepositoryImpl;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ * 操作日志表 - MongoDB
+ * </p>
+ *
+ * @author zhouchengjie
+ * @since 2020-07-19
+ */
+@Repository
+public class PcWebOperationLogRepositoryImpl extends MyBaseMongoRepositoryImpl<PcWebOperationLogMO, String> implements PcWebOperationLogRepository {
+
+
+}

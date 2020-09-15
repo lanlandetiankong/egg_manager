641956b9a420655b6ad0ca8451f5e82350fc9d32
lanlandetiankong <695605813@qq.com>
2020-09-15 22:38:56 +0800

    20200915 用户组件 Controller

---
+++ egg-web/src/main/java/com/egg/manager/web/controller/common/component/user/UserAccountCommonCompController.java
@@ -0,0 +1,66 @@
+package com.egg.manager.web.controller.common.component.user;
+
+import com.egg.manager.api.services.basic.user.UserAccountService;
+import com.egg.manager.common.annotation.log.pc.web.PcWebOperationLog;
+import com.egg.manager.common.annotation.user.CurrentLoginUser;
+import com.egg.manager.common.base.enums.base.BaseStateEnum;
+import com.egg.manager.common.base.pagination.antdv.AntdvPaginationBean;
+import com.egg.manager.common.base.pagination.antdv.AntdvSortBean;
+import com.egg.manager.common.base.query.form.QueryFormFieldBean;
+import com.egg.manager.persistence.bean.helper.MyCommonResult;
+import com.egg.manager.persistence.db.mysql.entity.user.UserAccount;
+import com.egg.manager.persistence.pojo.mysql.vo.user.UserAccountVo;
+import com.egg.manager.web.controller.BaseController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authz.annotation.Logical;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Slf4j
+@Api(value = "API ==>>  UserAccountCommonCompController ", description = "[通用组件]-用户账号接口")
+@RestController
+@RequestMapping("/commmon/component/user/user_account")
+public class UserAccountCommonCompController extends BaseController {
+
+    @Autowired
+    private UserAccountService userAccountService;
+
+
+    @PcWebOperationLog(action = "[通用组件]-查询用户信息-Dto列表", description = "[通用组件]-查询用户信息-Dto列表", fullPath = "/user/user_account/getAllUserAccountDtos",flag = false)
+    @ApiOperation(value = "[通用组件]-查询用户信息-Dto列表", notes = "[通用组件]-查询用户信息-Dto列表", response = MyCommonResult.class, httpMethod = "POST")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "queryObj", value = "字段查询配置 -> json格式", required = true, dataTypeClass = String.class),
+            @ApiImplicitParam(name = "paginationObj", value = "分页配置 -> json格式", required = true, dataTypeClass = String.class),
+            @ApiImplicitParam(name = "sortObj", value = "排序对象 -> json格式", required = true, dataTypeClass = String.class),
+    })
+    @PostMapping(value = "/getAllUserAccountDtos")
+    public MyCommonResult<UserAccountVo> doGetAllUserAccountDtos(HttpServletRequest request, String queryObj, String paginationObj, String sortObj,
+                                                                 @CurrentLoginUser UserAccount loginUser) {
+        MyCommonResult<UserAccountVo> result = new MyCommonResult<UserAccountVo>();
+        try {
+            //解析 搜索条件
+            List<QueryFormFieldBean> queryFormFieldBeanList = this.parseQueryJsonToBeanList(queryObj);
+            queryFormFieldBeanList.add(QueryFormFieldBean.dealGetEqualsBean("state", BaseStateEnum.ENABLED.getValue()));
+            //取得 分页配置
+            AntdvPaginationBean paginationBean = parsePaginationJsonToBean(paginationObj);
+            //取得 排序配置
+            List<AntdvSortBean> sortBeans = parseSortJsonToBean(sortObj, true);
+            result = userAccountService.dealGetUserAccountDtoPages(result, queryFormFieldBeanList, paginationBean, sortBeans);
+            dealCommonSuccessCatch(result, "查询用户信息-Dto列表:" + actionSuccessMsg);
+        } catch (Exception e) {
+            this.dealCommonErrorCatch(log, result, e);
+        }
+        return result;
+    }
+
+}

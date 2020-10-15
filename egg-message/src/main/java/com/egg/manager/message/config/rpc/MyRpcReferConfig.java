256862c038d85e9a13dca26ab1885a8f37635cab
lanlandetiankong <695605813@qq.com>
2020-10-15 18:09:28 +0800

    去除junit4

--- egg-message/src/main/java/com/egg/manager/message/config/rpc/MyRpcReferConfig.java
+++ egg-message/src/main/java/com/egg/manager/message/config/rpc/MyRpcReferConfig.java
@@ -17,4 +17,5 @@ public class MyRpcReferConfig {
     static class DubboReferConfig {
 
     }
+
 }
---
+++ egg-message/src/main/test/com/egg/manager/message/config/rpc/MyRpcReferConfigTest.java
@@ -0,0 +1,18 @@
+package com.egg.manager.message.config.rpc;
+
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * @Description:
+ * @ClassName: MyRpcReferConfigTest
+ * @Author: zhoucj
+ * @Date: 2020/10/15 18:05
+ */
+class MyRpcReferConfigTest {
+
+    @Test
+    public void test1() {
+        System.out.println("ok");
+    }
+}

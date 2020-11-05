a11f6a45d894f3f47456d772d3d503c9162bdd6b
lanlandetiankong <695605813@qq.com>
2020-11-05 11:33:18 +0800

    enhance与exchange调换

---
+++ egg-persistence/src/main/java/com/egg/manager/persistence/exchange/verification/igroup/VerifyGroupOfCreate.java
@@ -0,0 +1,9 @@
+package com.egg.manager.persistence.exchange.verification.igroup;
+
+/**
+ * @author zhoucj
+ * @description: 更新form 验证分组
+ * @date 2020/10/20
+ */
+public interface VerifyGroupOfCreate {
+}

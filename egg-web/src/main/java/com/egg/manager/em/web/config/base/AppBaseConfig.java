59e4d95fcb3c53df45d263d67ce0bed4fedc0a0b
lanlandetiankong <695605813@qq.com>
2020-12-31 10:58:57 +0800

    spring.profiles.active配置优化

--- egg-web/src/main/java/com/egg/manager/em/web/config/base/AppBaseConfig.java
+++ egg-web/src/main/java/com/egg/manager/em/web/config/base/AppBaseConfig.java
@@ -2,6 +2,7 @@ package com.egg.manager.em.web.config.base;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * @description: 自定义导入spring配置文件
@@ -11,13 +12,28 @@ import org.springframework.context.annotation.ImportResource;
 public class AppBaseConfig {
 
     /**
-     * 通过locations指定spring配置文件并加载
+     * 支持xml导入全局配置文件
      */
     @Configuration
     @ImportResource(locations = {
             //"classpath*:${egg.application.build.env}/dubbo/egg-message-dubboConfig.xml",
     })
     static class BaseXmlConfig {
+
+    }
+
+    /**
+     * 支持properties导入全局配置文件
+     * Application的更多配置引申(由spring.profiles.active指定改为当前配置)
+     * @note 只可使用classpath，不可使用classpath*，classpath*会导致找不到文件
+     *
+     */
+    @Configuration
+    @PropertySource(value = {
+            "classpath:config/application-${egg.application.build.env}-egg.properties",
+            "classpath:config/application-${egg.application.build.env}-universal.properties",
+    },encoding="UTF-8")
+    static class BasePropConfig {
 
     }
 }
